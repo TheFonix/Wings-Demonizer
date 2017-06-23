ae6164c4cfbfe21ca84d62d950f39956b1159d88
TheFonix <actualfriction@gmail.com>
2017-06-23 11:51:46 +0100

    Edited Script format!

--- daemonize.sh
+++ daemonize.sh
@@ -1,39 +1,39 @@
-echo ""
-echo ""
+  echo ""
+  echo ""
 echo "Written by Fonix 2017"
-echo ""
-echo ""
+  echo ""
+  echo ""
 echo "Script intended to be ran on Ubuntu, Not tested on other Linux OS's!"
 echo "Running Script"
 
 
 echo "Attempting to Daemonize wings for you!"
-sleep 3
+  sleep 3
 
 echo "Removing any current file"
-rm /etc/systemd/system/wings.service > /dev/null
-sleep 1
+  rm /etc/systemd/system/wings.service > /dev/null
+    sleep 1
 
 echo "Moving to the system services directory"
-cd /etc/systemd/system/
-sleep 1
+  cd /etc/systemd/system/
+    sleep 1
 
 echo "Downloading a pre-configured document for the wings service"
-wget https://fonix.online/repo/wings.service
-echo "File Injected"
-sleep 1
+  wget https://fonix.online/repo/wings.service
+    echo "File Injected"
+      sleep 1
 
 
 echo "Reloading systemctl daemon"
-systemctl daemon-reload
-sleep 1
+  systemctl daemon-reload
+  sleep 1
 
 echo "Enabling wings service"
-systemctl enable wings
+  systemctl enable wings
 
 echo "Daemon has Daemonized! Use the following commands to start, stop and restart the dameon:"
-echo ""
-echo ""
+  echo ""
+  echo ""
 echo "Restart = service wings restart"
 echo "Stop = service wings stop"
 echo "Start = service wings start"
